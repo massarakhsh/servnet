ec6a04db456b0aa64e7d2c50e90b8ee65e68fca5
System Administrator <root@shaman.rptp.org>
2020-12-17 08:17:33 +0300

    Shaman

--- task/baser/arper.go
+++ task/baser/arper.go
@@ -1,11 +1,13 @@
 package baser
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/massarakhsh/lik"
 	"github.com/massarakhsh/servnet/task"
 	"golang.org/x/crypto/ssh"
 	"io/ioutil"
+	"log"
 	"time"
 )
 
@@ -41,22 +43,37 @@ func (it *ARPer) DoStep() {
 
 func CallRouter() {
 	lik.SayInfo("SSH")
-	key, err := ioutil.ReadFile("root.opn")
-	if err != nil {
-		fmt.Println("BadRead: ", err)
+	if touch := Open("192.168.0.3:22", "admin", "", "root.opn"); touch != nil {
 	}
-	signer, err := ssh.ParsePrivateKey(key)
-	if err != nil {
-		fmt.Println("BadParse: ", err)
+}
+
+type LikSSH struct {
+	Server		string
+	ItClient	*ssh.Client
+	ItSession	*ssh.Session
+}
+
+func Open(server string, user string, password string, keyfile string) *LikSSH {
+	header := &LikSSH{ Server: server }
+	var auth []ssh.AuthMethod
+	if password != "" {
+		auth = append(auth, ssh.Password(password))
 	}
-	//var hostKey ssh.PublicKey
+	if keyfile != "" {
+		key, err := ioutil.ReadFile(keyfile)
+		if err != nil {
+			return nil
+		}
+		signer, err := ssh.ParsePrivateKey(key)
+		if err != nil {
+			return nil
+		}
+		auth = append(auth, ssh.PublicKeys(signer))
+	}
 	config := &ssh.ClientConfig{
-		User: "admin",
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
+		User: user,
+		Auth: auth,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		//HostKeyCallback: ssh.FixedHostKey(hostKey),
 	}
 	client, err := ssh.Dial("tcp", "192.168.0.3:22", config)
 	if err != nil {
@@ -64,6 +81,25 @@ func CallRouter() {
 		return
 	}
 	defer client.Close()
+
+	session, err := client.NewSession()
+	return header
+	return nil
+}
+
+func CallRouter2() {
+	if err != nil {
+		log.Fatal("Failed to create session: ", err)
+	}
+	defer session.Close()
+
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run("interface bridge host print without-paging"); err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+
 	lik.SayInfo("Ok")
 }
 
