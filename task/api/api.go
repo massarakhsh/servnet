0e94069e86c89cd59bbc6dc978578a55b8037a1a
Shaman <shamaner.17@gmail.com>
2021-01-10 23:44:48 +0300

    Shaman

--- task/api/api.go
+++ task/api/api.go
@@ -8,16 +8,18 @@ import (
 	"github.com/massarakhsh/servnet/task"
 	"log"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 )
+
+const ID = "SysNum"
 
 type APIer struct {
 	task.Task
 }
 
 type regTable struct {
+	Table	string
 	Elms	map[lik.IDB]*regElm
 }
 
@@ -35,15 +37,9 @@ var syncData = sync.Mutex{}
 func StartAPI() {
 	go func() {
 		apier := &APIer{}
-		apier.update()
 		apier.Initialize("APIer", apier)
 		go apier.runHttp()
 	}()
-}
-
-func (it *APIer) DoStep() {
-	it.update()
-	it.SetPause(time.Second * 15)
 }
 
 func (it *APIer) runHttp() {
@@ -53,69 +49,66 @@ func (it *APIer) runHttp() {
 	}
 }
 
-func (it *APIer) routerHttp(w http.ResponseWriter, r *http.Request) {
-	lik.SayInfo("Request: " + r.RequestURI)
-	json := Answer(0)
-	if strings.HasPrefix(r.RequestURI, "/api/stop") {
-		base.IsStoping = true
-	}
-	likapi.RouteJson(w, json, "relay=*")
+func (it *APIer) DoStep() {
+	it.updateAll()
+	it.SetPause(time.Second * 15)
 }
 
-func (it *APIer) update() {
-	UpdateBase([]string { "IPZone", "IP", "Ping", "Unit", "Link" }, "SysNum")
-}
-
-func UpdateBase(tables []string, key string) {
+func (it *APIer) updateAll() {
 	syncData.Lock()
+	for table,_ := range registerData {
+		it.updateTable(table)
+	}
+	syncData.Unlock()
+}
+
+func (it *APIer) updateTable(table string) {
 	idxnext := indexData
-	for _, table := range tables {
-		tbl := registerData[table]
-		if tbl == nil {
-			tbl = &regTable{}
-			tbl.Elms = make(map[lik.IDB]*regElm)
-			registerData[table] = tbl
+	tbl := registerData[table]
+	if tbl == nil {
+		tbl = &regTable{ Table: table }
+		tbl.Elms = make(map[lik.IDB]*regElm)
+		registerData[table] = tbl
+	}
+	if list := base.GetList(table); list != nil {
+		for _,val := range tbl.Elms {
+			val.Exists = false
 		}
-		if list := base.GetList(table); list != nil {
-			for _,val := range tbl.Elms {
-				val.Exists = false
-			}
-			for ne := 0; ne < list.Count(); ne++ {
-				if elm := list.GetSet(ne); elm != nil {
-					id := elm.GetIDB(key)
-					dt := elm.GetString("updated_at")
-					telm := tbl.Elms[id]
-					if telm == nil {
-						if idxnext == indexData {
-							idxnext++
-						}
-						telm = &regElm{}
-						telm.DT = dt
-						telm.Index = idxnext
-						telm.Exists = true
-						telm.Data = elm
-						tbl.Elms[id] = telm
-					} else if dt != telm.DT || telm.Data == nil {
-						if idxnext == indexData {
-							idxnext++
-						}
-						telm.DT = dt
-						telm.Index = idxnext
-						telm.Exists = true
-						telm.Data = elm
-					} else {
-						telm.Exists = true
-					}
-				}
-			}
-			for _,telm := range tbl.Elms {
-				if !telm.Exists && telm.Data != nil {
+		for ne := 0; ne < list.Count(); ne++ {
+			if elm := list.GetSet(ne); elm != nil {
+				id := elm.GetIDB(ID)
+				dt := elm.GetString("updated_at")
+				telm := tbl.Elms[id]
+				if telm == nil {
 					if idxnext == indexData {
 						idxnext++
 					}
+					telm = &regElm{}
+					telm.DT = dt
 					telm.Index = idxnext
-					telm.Data = nil
+					telm.Exists = true
+					telm.Data = elm
+					tbl.Elms[id] = telm
+				} else if dt != telm.DT || telm.Data == nil {
+					if idxnext == indexData {
+						idxnext++
+					}
+					telm.DT = dt
+					telm.Index = idxnext
+					telm.Exists = true
+					telm.Data = elm
+				} else {
+					telm.Exists = true
 				}
+			}
+		}
+		for _,telm := range tbl.Elms {
+			if !telm.Exists && telm.Data != nil {
+				if idxnext == indexData {
+					idxnext++
+				}
+				telm.Index = idxnext
+				telm.Data = nil
 			}
 		}
 	}
@@ -125,16 +118,49 @@ func UpdateBase(tables []string, key string) {
 		}
 		indexData = idxnext
 	}
-	syncData.Unlock()
 }
 
-func Answer(index int) lik.Seter {
+func (it *APIer) routerHttp(w http.ResponseWriter, r *http.Request) {
+	lik.SayInfo("Request: " + r.RequestURI)
+	names := lik.PathToNames(r.RequestURI)
+	if len(names) > 0 && names[0] == "api" {
+		it.routeAPI(w, names[1:])
+	}
+}
+
+func (it *APIer) routeAPI(w http.ResponseWriter, names []string) {
+	if len(names) > 0 && names[0] != "" {
+		table := names[0]
+		names = names[1:]
+		index := 0
+		if len(names) > 0 {
+			index = lik.StrToInt(names[0])
+			names = names[1:]
+		}
+		if answer := it.getTable(table, index); answer != nil {
+			likapi.RouteJson(w, answer, "relay=*")
+		}
+	}
+}
+
+func (it *APIer) getTable(table string, index int) lik.Seter {
 	syncData.Lock()
+	defer syncData.Unlock()
+	tbl := registerData[table]
+	if tbl == nil {
+		it.updateTable(table)
+		tbl = registerData[table]
+	}
+	answer := it.Answer(tbl, index)
+	return answer
+}
+
+func (it *APIer) Answer(tbl *regTable, index int) lik.Seter {
 	answer := lik.BuildSet()
 	answer.SetItem(indexData, "index")
-	for table,tbl := range registerData {
+	if tbl != nil {
 		anstab := lik.BuildSet()
-		answer.SetItem(anstab, table)
+		answer.SetItem(anstab, tbl.Table)
 		for id,telm := range tbl.Elms {
 			if telm.Index > index || index > indexData {
 				if telm.Data != nil {
@@ -145,7 +171,6 @@ func Answer(index int) lik.Seter {
 			}
 		}
 	}
-	syncData.Unlock()
 	return answer
 }
 
