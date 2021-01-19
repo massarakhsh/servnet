e015f69e29a89a19ed20484542bc4f1e20d000c8
Stanislav Mursalov <shamaner.17@gmail.com>
2021-01-19 09:59:34 +0300

    Shaman

--- base/touch.go
+++ base/touch.go
@@ -97,6 +97,9 @@ func TouchOnline(sysunit lik.IDB, port int, mac string, secs int) {
 		touch = AddTouch(0, sysunit, port, mac, at, at, ROLE_ONLINE)
 		if touch != nil {
 			touch.Update()
+			if DebugLevel > 0 {
+				lik.SayError("New Touch created")
+			}
 		}
 	}
 }
@@ -104,10 +107,13 @@ func TouchOnline(sysunit lik.IDB, port int, mac string, secs int) {
 func TouchTerminate() {
 	for _,touch := range TouchMapSys {
 		at := int(time.Now().Unix())
-		if (touch.Roles & ROLE_ONLINE) != 0 && at - touch.TimeAt > 2 * 60 {
+		if (touch.Roles & ROLE_ONLINE) != 0 && at - touch.TimeAt > 10 * 60 {
 			touch.Roles ^= ROLE_ONLINE
 			touch.Update()
-		} else if (touch.Roles & ROLE_ONLINE) == 0 && at - touch.TimeAt > 3 * 24 * 60 * 60 {
+		} else if (touch.Roles & ROLE_ONLINE) == 0 && at - touch.TimeAt > 3 * 24 * 60 * 60 {\
+			if DebugLevel > 0 {
+				lik.SayError("Old Touch daleted")
+			}
 			DeleteElm("Touch", touch.SysNum)
 			delete(TouchMapSys, touch.SysNum)
 		}
